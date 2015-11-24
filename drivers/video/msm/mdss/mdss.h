c0900f6d2f6751d9861f550676d2dd54a3efa6f7
feroxx <frhatfrat@gmail.com>
2015-11-25 00:41:22 +0200

    Update mdss.h

--- drivers/video/msm/mdss/mdss.h
+++ drivers/video/msm/mdss/mdss.h
@@ -102,6 +102,7 @@ struct mdss_data_type {
 	u32 has_bwc;
 	u32 has_decimation;
 	u8 has_wfd_blk;
+	atomic_t sd_client_count;
 	u8 has_wb_ad;
 
 	u32 mdp_irq_mask;
@@ -217,5 +218,13 @@ static inline int mdss_get_iommu_domain(u32 type)
 		return -ENODEV;
 
 	return mdss_res->iommu_map[type].domain_idx;
+}
+
+static inline int mdss_get_sd_client_cnt(void)
+{
+	if (!mdss_res)
+		return 0;
+	else
+		return atomic_read(&mdss_res->sd_client_count);
 }
 #endif /* MDSS_H */
