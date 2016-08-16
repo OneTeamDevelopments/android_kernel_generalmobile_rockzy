e11ef7c563d1230f51f28fbfe32c873c07183f52
feroxx <frhatfrat@gmail.com>
2016-08-16 18:20:25 +0300

    Update mdss_dsi_panel.c

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -1719,6 +1719,11 @@ int mdss_dsi_panel_init(struct device_node *node,
 {
 	int rc = 0;
 	static const char *panel_name;
+#if defined(CONFIG_GN_DEVICE_TYPE_CHECK) 
+	static const char *device_panel_name;
+	struct gn_device_info gn_mydev_info;
+	gn_mydev_info.gn_dev_type = GN_DEVICE_TYPE_LCD;
+#endif
 	struct mdss_panel_info *pinfo;
 
 	if (!node || !ctrl_pdata) {
@@ -1736,6 +1741,18 @@ int mdss_dsi_panel_init(struct device_node *node,
 	else
 		pr_info("%s: Panel Name = %s\n", __func__, panel_name);
 
+#if defined(CONFIG_GN_DEVICE_TYPE_CHECK) 
+	if(strstr(panel_name, "tianma"))
+		device_panel_name = "tianma_r63421";
+	else if(strstr(panel_name, "jdi"))
+		device_panel_name = "jdi_r63417";
+	else if(strstr(panel_name, "truly"))
+		device_panel_name = "truly_r63417";
+	else 
+		device_panel_name = "unknown lcd";
+	strcpy(gn_mydev_info.name, device_panel_name);
+	gn_set_device_info(gn_mydev_info);
+ #endif
 #ifdef CONFIG_VENDOR_EDIT
 	if(strstr(panel_name,"rsp 1440p video mode dsi panel") || strstr(panel_name,"rsp 1440p cmd mode dsi panel"))
 		find7s_lcd_rsp_ic = 1;
