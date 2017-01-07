d8acfc92509dce8fe0d445cc564b6585bab6df16
feroxx <frhatfrat@gmail.com>
2017-01-07 02:11:45 +0200

    Update msm_sensor.h

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor.h
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor.h
@@ -42,40 +42,6 @@ enum msm_sensor_state_t {
 	MSM_SENSOR_POWER_UP,
 };
 
-struct gn_sunny_ov4688_otp_struct   
-{
-    int16_t customer_id;
-	int16_t module_integrator_id;
-	int16_t lens_id;
-	int16_t rg_ratio;
-	int16_t bg_ratio;
-	int16_t light_rg;
-	int16_t light_bg;
-	int16_t production_year;
-	int16_t production_month;
-	int16_t production_day;
-	int16_t VCM_end;
-	int16_t VCM_start;
-	int16_t user_data[5];
-};
-
-struct gn_sunny_ov8835_otp_struct   
-{
-    int16_t customer_id;
-	int16_t module_integrator_id;
-	int16_t lens_id;
-	int16_t rg_ratio;
-	int16_t bg_ratio;
-	int16_t light_rg;
-	int16_t light_bg;
-	int16_t user_data[5];
-	int16_t lenc[63];//63
-};
-
-struct gn_otp_sensor_fn_t {
-        int (*gn_sensor_otp_support)(struct msm_sensor_ctrl_t *s_ctrl);
-};
-
 struct msm_sensor_fn_t {
 	int (*sensor_config) (struct msm_sensor_ctrl_t *, void __user *);
 	int (*sensor_power_down)
@@ -107,7 +73,6 @@ struct msm_sensor_ctrl_t {
 	uint16_t clk_info_size;
 	void *misc_regulator;
 	enum msm_sensor_state_t sensor_state;
-    struct gn_otp_sensor_fn_t *gn_otp_func_tbl;
 };
 
 int32_t msm_sensor_config(struct msm_sensor_ctrl_t *s_ctrl,
@@ -127,15 +92,4 @@ int32_t msm_sensor_i2c_probe(struct i2c_client *client,
 
 int32_t msm_sensor_free_sensor_data(struct msm_sensor_ctrl_t *s_ctrl);
 
-int32_t msm_sensor_get_dt_gpio_req_tbl(struct device_node *of_node,
-	struct msm_camera_gpio_conf *gconf, uint16_t *gpio_array,
-	uint16_t gpio_array_size);
-
-int32_t msm_sensor_get_dt_gpio_set_tbl(struct device_node *of_node,
-	struct msm_camera_gpio_conf *gconf, uint16_t *gpio_array,
-	uint16_t gpio_array_size);
-
-int32_t msm_sensor_init_gpio_pin_tbl(struct device_node *of_node,
-	struct msm_camera_gpio_conf *gconf, uint16_t *gpio_array,
-	uint16_t gpio_array_size);
 #endif
