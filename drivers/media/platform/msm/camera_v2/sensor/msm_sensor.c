2de57148972a2895a9075684f8ced59810844f58
feroxx <frhatfrat@gmail.com>
2017-01-31 19:47:02 +0200

    Update msm_sensor.c

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
@@ -27,7 +27,6 @@
 #define CDBG(fmt, args...) do { } while (0)
 #endif
 
-int32_t CamState = 0; 
 static int32_t msm_sensor_enable_i2c_mux(struct msm_camera_i2c_conf *i2c_conf)
 {
 	struct v4l2_subdev *i2c_mux_sd =
@@ -1671,7 +1670,6 @@ int32_t msm_sensor_config(struct msm_sensor_ctrl_t *s_ctrl,
 				break;
 			}
 			s_ctrl->sensor_state = MSM_SENSOR_POWER_UP;
-			CamState = 1; 
 			pr_err("%s:%d sensor state %d\n", __func__, __LINE__,
 				s_ctrl->sensor_state);
 		} else {
@@ -1699,8 +1697,7 @@ int32_t msm_sensor_config(struct msm_sensor_ctrl_t *s_ctrl,
 					__LINE__, rc);
 				break;
 			}
-			s_ctrl->sensor_state = MSM_SENSOR_POWER_DOWN;
-			CamState = 0; 
+			s_ctrl->sensor_state = MSM_SENSOR_POWER_DOWN; 
 			pr_err("%s:%d sensor state %d\n", __func__, __LINE__,
 				s_ctrl->sensor_state);
 		} else {
