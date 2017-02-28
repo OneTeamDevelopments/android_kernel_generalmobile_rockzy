3e7428f86bdc739d28ff78bdb3c207eacad21595
feroxx <frhatfrat@gmail.com>
2017-02-28 21:48:46 +0200

    Update mdss_dsi_panel.c

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -1046,7 +1046,7 @@ int mdss_dsi_panel_init(struct device_node *node,
 		device_panel_name = "truly_r63417";
 	else if(strstr(panel_name, "sharp"))
 		device_panel_name = "sharp_r63417";
-	else if(strstr(panel_name, "sharp r63419"))
+	else if(strstr(panel_name, "sharp Dual"))
 		device_panel_name = "sharp_r63419";
 	else 
 		device_panel_name = "unknown lcd";
