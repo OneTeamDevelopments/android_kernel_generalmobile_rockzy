6412f1f587c570fb25177debc0fa827f738328bf
feroxx <frhatfrat@gmail.com>
2017-04-05 15:57:10 +0300

    forgotten one

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor.h
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor.h
@@ -45,6 +45,27 @@ enum msm_sensor_state_t {
 	MSM_SENSOR_POWER_UP,
 };
 
+struct gn_sunny_ov4688_otp_struct   
+{
+    int16_t customer_id;
+	int16_t module_integrator_id;
+	int16_t lens_id;
+	int16_t rg_ratio;
+	int16_t bg_ratio;
+	int16_t light_rg;
+	int16_t light_bg;
+	int16_t production_year;
+	int16_t production_month;
+	int16_t production_day;
+	int16_t VCM_end;
+	int16_t VCM_start;
+	int16_t user_data[5];
+};
+
+struct gn_otp_sensor_fn_t {
+        int (*gn_sensor_otp_support)(struct msm_sensor_ctrl_t *s_ctrl);
+};
+
 struct msm_sensor_fn_t {
 	int (*sensor_config) (struct msm_sensor_ctrl_t *, void __user *);
 	int (*sensor_power_down) (struct msm_sensor_ctrl_t *);
@@ -77,6 +98,7 @@ struct msm_sensor_ctrl_t {
 	struct device_node *of_node;
 	enum msm_camera_stream_type_t camera_stream_type;
 	uint32_t set_mclk_23880000;
+	struct gn_otp_sensor_fn_t *gn_otp_func_tbl;
 };
 
 int msm_sensor_config(struct msm_sensor_ctrl_t *s_ctrl, void __user *argp);
