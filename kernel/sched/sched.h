bc115cb9e4e8cc42f9fdffd9c756e9813c7a52e3
feroxx <frhatfrat@gmail.com>
2018-02-16 12:57:14 +0300

    back to cpuquiet

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -378,6 +378,11 @@ struct rq {
 #endif
 	int skip_clock_update;
 
+	/* time-based average load */
+	u64 nr_last_stamp;
+	unsigned int ave_nr_running;
+	seqcount_t ave_seqcnt;
+
 	/* capture load from *all* tasks on this cpu: */
 	struct load_weight load;
 	unsigned long nr_load_updates;
